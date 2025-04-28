e780f4cf2b0a37840447b1fd5e30731d24824815
MortalTurtle <ars.lukin@ya.ru>
2025-04-28 22:20:37 +0500

    text selection added

--- readme.md
+++ readme.md
@@ -9,16 +9,18 @@ multi-user text editor
 
 #internal message format:
 
-    [sender_username] -E [printed] / user edited
+    ''[sender_username] -E [printed]'' / user edited
 
-    [sender_username] -M [user_x] [user_y] / user position shifted
+    ''[sender_username] -M [direction]'' / user position shifted, [direction] can be only 'l'/'r'/'u'/'d'
 
-    [sender_username] -T [text] / file text
+    ''[sender_username] -MS [direction]'' / user position shifted with SHIFT pressed, [direction] can be only 'l'/'r'/'u'/'d'
 
-    [sender_username] -U ([username] [user_x] [user_y])*  / users in session
+    ''[sender_username] -T [text]'' / file text
 
-    [sender_username] -C [sender_username] / new user connected to session
+    ''[sender_username] -U ([username] [user_x] [user_y])*''  / users in session
 
-    [sender_username] -D / user deleted char
+    ''[sender_username] -C [sender_username]'' / new user connected to session
 
-    [sender_username] -NL / user added new line
+    ''[sender_username] -D'' / user deleted char
+
+    ''[sender_username] -NL'' / user added new line
--- src/main.py
+++ src/main.py
@@ -1,27 +1,26 @@
 import re
 from mttext_app import MtTextEditApp
 import sys
+import argparse
 
 
-def connect_to_session(debug, offset):
+def connect_to_session(debug, conn_ip, username):
     r = re.compile(r"(\d{1,3}\.){3}\d{1,3}")
-    conn_ip = sys.argv[2 + offset]
     if not r.match(conn_ip):
         print("Wrong connection ip address")
         return 0
-    socket = MtTextEditApp(sys.argv[3 + offset], debug=debug)
+    socket = MtTextEditApp(username, debug=debug)
     socket.connect(conn_ip)
 
 
-def host_session(debug, offset):
-    file_path = sys.argv[2 + offset]
+def host_session(debug, file_path, username):
     try:
         with open(file_path, 'r') as f:
             filetext = f.read()
     except IOError:
         print("File does not exist :(")
         return
-    socket = MtTextEditApp(sys.argv[3 + offset], filetext,
+    socket = MtTextEditApp(username, filetext,
                            debug=debug, file_path=file_path)
     socket.run()
 
@@ -29,13 +28,29 @@ def host_session(debug, offset):
 def main():
     offset = 0
     debug = False
-    if sys.argv[1] == '-D':
-        offset += 1
-        debug = True
-    if sys.argv[1 + offset] == '-C':
-        connect_to_session(debug, offset)
-    if sys.argv[1 + offset] == '-H':
-        host_session(debug, offset)
+    parser = argparse.ArgumentParser(
+        prog="mtrtext",
+        description="multi-user text editor",
+        epilog=":)",
+        usage="%(prog)s [-D] (-H FILE_PATH USERNAME | -C CONN_IP USERNAME)"
+    )
+    parser.add_argument('-D', action='store_true', default=False,
+                        dest='debug',
+                        help="Print some debug messages")
+    parser.add_argument('-H', nargs=2,
+                        metavar=('FILE_PATH', 'USERNAME'),
+                        help='Host edit session')
+    parser.add_argument('-C', nargs=2,
+                        metavar=('CONN_IP', 'USERNAME'),
+                        help='Connect to session')
+    try:
+        args = parser.parse_args()
+        if args.C:
+            connect_to_session(args.debug, args.C[0], args.C[1])
+        if args.H:
+            host_session(args.debug, args.H[0], args.H[1])
+    except:
+        parser.print_help()
 
 
 if __name__ == '__main__':
