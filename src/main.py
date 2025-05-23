52571a7b99a3d917a4ec9a5794b61470b008ee67
Rinarinaaaa <rinaanashova@yandex.ru>
2025-05-23 15:52:04 +0500

    pep8

--- src/main.py
+++ src/main.py
@@ -5,35 +5,41 @@ import sys
 import argparse
 import os
 
-PERMISSION_FILE = '/tmp/lib/mttext/permissions'
+PERMISSION_FILE = "/tmp/lib/mttext/permissions"
 HISTORY_FILE_PATH = "/tmp/lib/mttext/history/"
 
 # TODO: implement correct division for files with same filename
 
 
 def list_all_saved_history(file_path):
-    file_name = file_path[file_path.rfind('/'):]
-    os.makedirs(os.path.dirname(HISTORY_FILE_PATH +
-                file_name + '/'), exist_ok=True)
-    files = os.listdir(HISTORY_FILE_PATH + file_name + '/')
+    file_name = file_path[file_path.rfind("/") :]
+    os.makedirs(
+        os.path.dirname(HISTORY_FILE_PATH + file_name + "/"), exist_ok=True
+    )
+    files = os.listdir(HISTORY_FILE_PATH + file_name + "/")
     files.sort()
     i = 1
-    for hist_file in filter(lambda x: '.o.cache' not in x, files):
-        print(hist_file[:hist_file.rfind('.')] + f'\t{i}')
+    for hist_file in filter(lambda x: ".o.cache" not in x, files):
+        print(hist_file[: hist_file.rfind(".")] + f"\t{i}")
         i += 1
 
 
 def show_changes(file_path, changes_index):
-    file_name = file_path[file_path.rfind('/'):]
-    file_list = os.listdir(
-        HISTORY_FILE_PATH + file_name + '/')
+    file_name = file_path[file_path.rfind("/") :]
+    file_list = os.listdir(HISTORY_FILE_PATH + file_name + "/")
     file_list.sort()
-    files = list(filter(lambda x: '.o.cache' in x, file_list))
+    files = list(filter(lambda x: ".o.cache" in x, file_list))
     try:
-        with open(HISTORY_FILE_PATH + file_name + '/' + files[int(changes_index) - 1], 'r') as f:
+        with open(
+            HISTORY_FILE_PATH
+            + file_name
+            + "/"
+            + files[int(changes_index) - 1],
+            "r",
+        ) as f:
             filetext = f.read()
     except:
-        print('no such changes file found, :(')
+        print("no such changes file found, :(")
         return
     app = MtTextEditApp("view_changes", filetext)
     app.show_changes(file_name, files[int(changes_index) - 1])
@@ -42,10 +48,10 @@ def show_changes(file_path, changes_index):
 def get_permissions():
     permissions = {}
     try:
-        with open(PERMISSION_FILE, 'r') as f:
+        with open(PERMISSION_FILE, "r") as f:
             for line in f:
-                if ':' in line:
-                    user, rights = line.strip().split(':')
+                if ":" in line:
+                    user, rights = line.strip().split(":")
                     permissions[user] = rights
     except FileNotFoundError:
         os.makedirs(os.path.dirname(PERMISSION_FILE), exist_ok=True)
@@ -61,17 +67,20 @@ def list_permissions():
 def manage_permissions(username, access_rights):
     sign = access_rights[0]
     access_rights = access_rights[1:]
-    if access_rights not in ['rw', 'r'] or (sign != '+' and sign != '-'):
+    if access_rights not in ["rw", "r"] or (sign != "+" and sign != "-"):
         return False
     try:
         permissions = get_permissions()
-        if sign == '+':
-            if access_rights == 'rw' or access_rights == 'r' and \
-                    permissions.get(username, '') != 'rw':
+        if sign == "+":
+            if (
+                access_rights == "rw"
+                or access_rights == "r"
+                and permissions.get(username, "") != "rw"
+            ):
                 permissions[username] = access_rights
         elif username in permissions:
             permissions.pop(username)
-        with open(PERMISSION_FILE, 'w') as f:
+        with open(PERMISSION_FILE, "w") as f:
             for user, rights in permissions.items():
                 f.write(f"{user}:{rights}\n")
         return True
@@ -91,13 +100,14 @@ def connect_to_session(debug, conn_ip, username):
 
 def host_session(debug, file_path, username):
     try:
-        with open(file_path, 'r') as f:
+        with open(file_path, "r") as f:
             filetext = f.read()
     except IOError:
         print("File does not exist :(")
         return
-    socket = MtTextEditApp(username, filetext,
-                           debug=debug, file_path=file_path)
+    socket = MtTextEditApp(
+        username, filetext, debug=debug, file_path=file_path
+    )
     socket.run()
 
 
@@ -109,28 +119,48 @@ def main():
         description="multi-user text editor",
         epilog=":)",
         usage="%(prog)s [-D] (-H FILE_PATH USERNAME | -C CONN_IP USERNAME | \
-        -P USERNAME ACCESS_RIGHTS | -Pl | -CHH FILE_PATH | -CH FILE_PATH INDEX)"
+        -P USERNAME ACCESS_RIGHTS | -Pl | -CHH FILE_PATH | -CH FILE_PATH INDEX)",
     )
-    parser.add_argument('-D', action='store_true', default=False,
-                        dest='debug',
-                        help="Print some debug messages")
-    parser.add_argument('-H', nargs=2,
-                        metavar=('FILE_PATH', 'USERNAME'),
-                        help='Host edit session')
-    parser.add_argument('-C', nargs=2,
-                        metavar=('CONN_IP', 'USERNAME'),
-                        help='Connect to session')
-    parser.add_argument('-P', nargs=2,
-                        metavar=('USERNAME', 'ACCESS_RIGHTS'),
-                        help='Manage user permissions + to add, - to remove (rw - read/write, r - read only)')
-    parser.add_argument('-Pl', action='store_true', default=False,
-                        help="List all permissions")
-    parser.add_argument('-CHH', nargs=1,
-                        metavar=('FILE_PATH'),
-                        help='List all availible history changes for file')
-    parser.add_argument('-CH', nargs=2,
-                        metavar=('FILE_PATH', 'INDEX'),
-                        help='Show history for file from i-th session')
+    parser.add_argument(
+        "-D",
+        action="store_true",
+        default=False,
+        dest="debug",
+        help="Print some debug messages",
+    )
+    parser.add_argument(
+        "-H",
+        nargs=2,
+        metavar=("FILE_PATH", "USERNAME"),
+        help="Host edit session",
+    )
+    parser.add_argument(
+        "-C",
+        nargs=2,
+        metavar=("CONN_IP", "USERNAME"),
+        help="Connect to session",
+    )
+    parser.add_argument(
+        "-P",
+        nargs=2,
+        metavar=("USERNAME", "ACCESS_RIGHTS"),
+        help="Manage user permissions + to add, - to remove (rw - read/write, r - read only)",
+    )
+    parser.add_argument(
+        "-Pl", action="store_true", default=False, help="List all permissions"
+    )
+    parser.add_argument(
+        "-CHH",
+        nargs=1,
+        metavar=("FILE_PATH"),
+        help="List all availible history changes for file",
+    )
+    parser.add_argument(
+        "-CH",
+        nargs=2,
+        metavar=("FILE_PATH", "INDEX"),
+        help="Show history for file from i-th session",
+    )
     try:
         args = parser.parse_args()
         if args.Pl:
@@ -149,5 +179,5 @@ def main():
         parser.print_help()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
